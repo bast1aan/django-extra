5756e523962604cabc0e7f801258166ad10c2932
Bastiaan Welmers <bastiaan@welmers.net>
2020-05-12 22:10:29 +0200

    Added unit package for clean isolated tests

---
+++ tests/bast1aan/django_extra/unit/__init__.py
@@ -0,0 +1,7 @@
+"""
+	Module for storing 'real' _unit_ tests.
+	Meaning, they should test single small units e.g. methods or functions in an isolated way.
+
+	Try to use unittest.TestCase or django.test.SimpleTestCase instead of django.test.TestCase
+	to prevent unnecessary database setup and improve speed.
+"""
